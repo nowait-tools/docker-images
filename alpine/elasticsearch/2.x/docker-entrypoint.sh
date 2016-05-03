a1a70c1befabec2bf8435c9d1c3439efd25b187c
Michael Schroeder <m.schroeder223@gmail.com>
2016-05-03 16:03:31 -0400

    Moved env variables to config file

--- alpine/elasticsearch/2.x/docker-entrypoint.sh
+++ alpine/elasticsearch/2.x/docker-entrypoint.sh
@@ -1,33 +1,11 @@
 #!/bin/bash
 set -e
 
-OPTS="$OPTS -Des.transport.tcp.port=9300 -Des.http.port=9200"
-
-if [ -n "$ES_CLUSTER_NAME" ]; then
-  OPTS="$OPTS -Des.cluster.name=$ES_CLUSTER_NAME"
-fi
-
-if [ -n "$ES_MULTICAST" ]; then
-  OPTS="$OPTS -Des.discovery.zen.ping.multicast.enabled=$ES_MULTICAST"
-fi
-
-if [ -n "$ES_MASTER" ]; then
-  OPTS="$OPTS -Dnode.master=$ES_MASTER"
-fi
-
-if [ -n "$ES_DATA" ]; then
-  OPTS="$OPTS -Dnode.data=$ES_DATA"
-fi
-
-if [ -n "$ES_DATA_PATH" ]; then
-  OPTS="$OPTS -Dpath.data=$ES_DATA_PATH"
-fi
-
 # Compile config file templates
 su-exec elasticsearch:elasticsearch confd -onetime -backend=$CONFD_BACKEND -prefix=/latest -confdir=/opt/elasticsearch
 
 # Add elasticsearch as command if needed
-if [[ $1 == -* ]]; then
+if [ "${1:0:1}" = '-' ]; then
 	set -- elasticsearch "$@"
 fi
 
@@ -36,12 +14,10 @@ fi
 if [ "$1" = 'elasticsearch' -a "$(id -u)" = '0' ]; then
 	# Change the ownership of /opt/elasticsearch/data to elasticsearch
 	chown -R elasticsearch:elasticsearch /opt/elasticsearch/data
-
-	set -- su-exec elasticsearch "$@"
-	#exec su-exec elasticsearch "$BASH_SOURCE" "$@"
+	set -- su-exec elasticsearch:elasticsearch "$@"
 fi
 
 # As argument is not related to elasticsearch,
 # then assume that user wants to run his own process,
 # for example a `bash` shell to explore this image
-exec "$@" "$OPTS"
+exec "$@"

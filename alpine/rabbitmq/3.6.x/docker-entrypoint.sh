5c8c5c6e64e9ba584a09f5124794aa68bf9fa46b
Michael Schroeder <m.schroeder223@gmail.com>
2016-05-24 10:48:54 -0400

    Removes duplicate ownership setting from /opt/rabbitmq/plugins

--- alpine/rabbitmq/3.6.x/docker-entrypoint.sh
+++ alpine/rabbitmq/3.6.x/docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-if [ -d "/plugins" ]; then
-  chown -R rabbitmq:rabbitmq /opt/rabbitmq/plugins
-fi
-
 if [[ $EUID -eq 0 ]]; then
   exec su-exec rabbitmq:rabbitmq /opt/scripts/server.sh "$@"
 else

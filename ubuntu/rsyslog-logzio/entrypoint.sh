b03f60f1d56d4cc904bbb12e67f9e2a1e0ee0dac
Dom Del Nano <ddelnano@gmail.com>
2017-05-03 23:24:46 -0400

    Allow hostname to be configured via an env

--- ubuntu/rsyslog-logzio/entrypoint.sh
+++ ubuntu/rsyslog-logzio/entrypoint.sh
@@ -2,5 +2,6 @@
 
 sed -i "s/%LOGZIO_TOKEN%/$LOGZIO_TOKEN/g" /etc/rsyslog.conf
 sed -i "s/%TYPE%/$TYPE/g" /etc/rsyslog.conf
+sed -i "s/%HOSTNAME%/$HOST_HOSTNAME/g" /etc/rsyslog.conf
 
 /usr/sbin/rsyslogd -n

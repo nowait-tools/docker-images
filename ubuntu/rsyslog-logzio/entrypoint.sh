98daae9d4da65e385e8ac46f9f5efe20a6080f74
Dom Del Nano <ddelnano@gmail.com>
2017-05-04 07:59:45 -0400

    Use bind mounted /etc/hostname to find correct hostname without an env

--- ubuntu/rsyslog-logzio/entrypoint.sh
+++ ubuntu/rsyslog-logzio/entrypoint.sh
@@ -2,6 +2,7 @@
 
 sed -i "s/%LOGZIO_TOKEN%/$LOGZIO_TOKEN/g" /etc/rsyslog.conf
 sed -i "s/%TYPE%/$TYPE/g" /etc/rsyslog.conf
+HOST_HOSTNAME=$(echo /etc/hostname)
 sed -i "s/%HOSTNAME%/$HOST_HOSTNAME/g" /etc/rsyslog.conf
 
 /usr/sbin/rsyslogd -n
